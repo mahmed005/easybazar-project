a4d1835f21ce8440a5c75add1ef32c8d8b49d3f0
Muhammad Ahmed <mahmed@bwop@gmail.com>
2024-04-23 15:21:01 +0500

    Changed backend.js file

---
+++ backend.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+const ejs = require("ejs");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended : true}));
+
+app.set("view engine" , ejs);
+
+app.listen(3000 , () => {
+    console.log("Server is running on port 3000");
+});
