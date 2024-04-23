fcb2e884b86aa3ab8e170e56befa76793696b18d
Muhammad Ahmed <mahmed@bwop@gmail.com>
2024-04-23 23:11:54 +0500

    Added backend functionality

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
 const ejs = require("ejs");
+const database = require(__dirname + "/database.js");
 
 const app = express();
 
@@ -14,10 +15,26 @@ app.listen(3000 , () => {
 });
 
 app.get("/login" , (req,res) => {
-
+    res.sendFile(__dirname + "/views/LoginandSignup.html");
 })
 
-app.post("/login" , (req,res) => {
-    const {email,password,type} = req.body;  
-    
+app.post("/login" , async (req,res) => {
+    const {email,password} = req.body;  
+    const result = await database.getPassword(email);
+    const isValid = await  bcrypt.compare(password , result[0].password);
+    if(isValid)
+    {
+        res.send("Login successfull");
+        return;
+    }
+    else
+    {
+        res.send("Login failed");
+    }
+})
+
+app.get("/signup" , async (req,res) => {
+    const password = await bcrypt.hash("ahmed" , 10);
+    const result = await database.enterRecord(password);
+    console.log(result);
 })
